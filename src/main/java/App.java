f1ffe50694771ac3284251db87e65a842d33a437
kara4k <sif4bag2>
2020-09-02 18:45:03 +0300

    commit pair 1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Test");
         System.out.println("Test");
-        System.out.println("Test");
     }
 }
