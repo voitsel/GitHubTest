d8478487aeae7c151f063d0e240bf6edd6ea7654
kara4k <sif4bag2>
2020-09-25 13:51:08 +0300

    Refactorings 3

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -2,6 +2,5 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("hjhj");
         System.out.println("hjhj");
-        System.out.println("hjhj");
     }
 }
