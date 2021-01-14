1450543e954407626ff1e2a2df4c7e9ff5bf7ad4
Bob <bob@mail.com>
2021-01-14 16:08:20 +0300

    BRANCH-44 initial

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -1,11 +1,5 @@
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
-        System.out.println("Test");
     }
 }
